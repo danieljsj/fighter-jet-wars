2d6659f6302a3b05715367425cb00ea66b21ee21
Daniel <danielschulzjackson@gmail.com>
2015-03-23 22:34:05 -0500

    enabled computers

--- js/begin.js
+++ js/begin.js
@@ -1,19 +1,22 @@
 // Let's play this game!
 
 
-console.info('PLAYERS BEFORE INIT:');
+console.info('PLANES BEFORE INIT:');
 console.log(player1);
 console.log(player2);
-//console.log(comp1);
+console.log(comp1);
+console.log(comp1);
 
 player1		.init();
 player2		.init();
-//comp1		.init();
+comp1		.init();
+comp1		.init();
 
-console.info('PLAYERS AFTER INIT:');
+console.info('PLANES AFTER INIT:');
 console.log(player1);
 console.log(player2);
-//console.log(comp1);
+console.log(comp1);
+console.log(comp2);
 
 var then = Date.now();
 
