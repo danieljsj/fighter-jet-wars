d5b03ef28365b2fe5a89065f8319b7880c8c3102
Daniel <danielschulzjackson@gmail.com>
2015-03-23 22:46:50 -0500

    active: 2 computers plus player 2. - comps fire when facing to or away from player (should be only to) - comps just spin in circles

--- js/begin.js
+++ js/begin.js
@@ -1,22 +1,23 @@
 // Let's play this game!
 
 
-console.info('PLANES BEFORE INIT:');
-console.log(player1);
+// console.log(player1);
+// 			player1.init();
+// console.log(player1);
+
 console.log(player2);
+			player2.init();
+console.log(player2);
+
 console.log(comp1);
+			comp1.init();
 console.log(comp1);
 
-player1		.init();
-player2		.init();
-comp1		.init();
-comp1		.init();
-
-console.info('PLANES AFTER INIT:');
-console.log(player1);
-console.log(player2);
-console.log(comp1);
 console.log(comp2);
+			comp2.init();
+console.log(comp2);
+
+
 
 var then = Date.now();
 
