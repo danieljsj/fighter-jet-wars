c761fe202dcc58f36aa6c2b85861aad960acb634
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-06-22 20:43:09 -0500

    implementing old firebase in server -- working

--- angularfire/_server/server.js
+++ angularfire/_server/server.js
@@ -2,73 +2,88 @@
 // "lead"; figure out in general how far behind the server (not firebase) time, and render everything at where it theoretically is on the server. (whoah... wait... does that work? show me the current state? or better yet - show me a predicted future state -- a state as far ahead of the server's reality as my commands are from reaching the server. which will mean some jumpiness... not too fancy; each plane's draw() function would project it ahead by a dT.)
 // rebuild the whole thing without firebase, with only websockets (hosting costs?!)
 
-var firebase = require('firebase');
+var Firebase = require('firebase');
 
 // console.log("|||");
-// console.log(firebase);
+// console.log(Firebase);
 // console.log("|||");
 // console.log(firebase.initializeApp);
 // console.log("|||");
 
-var firebaseConfig = {
+// should be its own service:
+var FirebaseTokenGenerator = require("firebase-token-generator");
+var fbSecret = 'M6mwQVaUkf0wrVd0I0aQIvM5QS1TW5Z2hbNguWol';
+var tokenGenerator = new FirebaseTokenGenerator(fbSecret);
+var token = tokenGenerator.createToken(
+   {uid: "my-awesome-server"}, 
+   {expires: 3600*24}
+);
+var ref = new Firebase("https://fighter-jets-oldfire.firebaseio.com");
+ref.authWithCustomToken(token, function(error, authData) {
 
-	
-	// SHOULD BE GOOD:
-	serviceAccount: "server/config/auth-config-default.json",
-	databaseURL: "https://fighter-jets-oldfire.firebaseio.com",
-	
-	// MIGHT ALSO WORK:
-	// projectId: "fighter-jets",
- 	// clientEmail: "game-server-0@fighter-jets.iam.gserviceaccount.com",
- 	// privateKey: "-----BEGIN PRIVATE KEY-----lalalalal\n-----END PRIVATE KEY-----\n",
-	
+	console.log(error,authData);
+  
+	var p1Ref = ref.child("players/1");
 
-	// NOT NEEDED:
-	// apiKey: "AIzaSyB0wRqUQWX0nfOl7TC8ydGgs0MGXnLJ_9Y", // no good on server
-	// authDomain: "fighter-jets.firebaseapp.com", // not relevant
-	// storageBucket: "fighter-jets.appspot.com", // not relevant
-};
+	var p1 = {
+		controls: 	{turningRight:false,turningLeft:false,accellerating:false,braking:false},
+		p: 			{x:0,y:0,direction:0,speed:0},
+	};
 
-/*var app = */firebase.initializeApp(firebaseConfig);
+	p1Ref.on('value', function(p1Snapshot){
+		p1.controls = p1Snapshot.val().controls;
+		p1.p = p1Snapshot.val().p;
+	});
 
 
+	var lastTime = new Date().getTime();
+	var p1LastTimeJSON = JSON.stringify(p1);
+	var p1LastTime = JSON.parse(p1LastTimeJSON);
+	setInterval(function(){
 
+		var newTime = new Date().getTime();
+		var dT = newTime - lastTime;
+		lastTime = newTime;
 
-var p1Ref = firebase.database().ref("players/1");
+		if (p1.p && p1.controls){
+			p1.p.x += dT * (p1.controls.turningRight - p1.controls.turningLeft);
+			p1.p.y += dT * (p1.controls.braking - p1.controls.accellerating); // needs some renames here...
 
-var p1 = {
-	controls: 	null,
-	p: 			null,
-};
+			var newJSON = JSON.stringify(p1.p);
 
-p1Ref.on('value', function(p1Snapshot){
-	p1.controls = p1Snapshot.val().controls;
-	p1.p = p1Snapshot.val().p;
+			if ( newJSON !== p1LastTimeJSON ) {
+				// console.log(newJSON + '!==' + p1LastTimeJSON);
+				console.log('sending '+ newJSON);
+				p1Ref.child('p').set(p1.p);
+				p1LastTimeJSON = newJSON;
+				p1LastTime = JSON.parse(p1LastTimeJSON);
+			}
+		}
+
+	},1);
+
 });
 
 
-var lastTime = new Date().getTime();
-var p1LastTimeJSON = JSON.stringify(p1);
-var p1LastTime = JSON.parse(p1LastTimeJSON);
-setInterval(function(){
+// var firebaseConfig = {
 
-	var newTime = new Date().getTime();
-	var dT = newTime - lastTime;
-	lastTime = newTime;
+	
+// 	// SHOULD BE GOOD:
+// 	serviceAccount: "../../server/config/auth-config-default.json",
+// 	databaseURL: "https://fighter-jets-oldfire.firebaseio.com",
+	
+// 	// MIGHT ALSO WORK:
+// 	// projectId: "fighter-jets",
+//  	// clientEmail: "game-server-0@fighter-jets.iam.gserviceaccount.com",
+//  	// privateKey: "-----BEGIN PRIVATE KEY-----lalalalal\n-----END PRIVATE KEY-----\n",
+	
 
-	if (p1.p && p1.controls){
-		p1.p.x += dT * (p1.controls.turningRight - p1.controls.turningLeft);
-		p1.p.y += dT * (p1.controls.braking - p1.controls.accellerating); // needs some renames here...
+// 	// NOT NEEDED:
+// 	// apiKey: "AIzaSyB0wRqUQWX0nfOl7TC8ydGgs0MGXnLJ_9Y", // no good on server
+// 	// authDomain: "fighter-jets.firebaseapp.com", // not relevant
+// 	// storageBucket: "fighter-jets.appspot.com", // not relevant
+// };
 
-		var newJSON = JSON.stringify(p1.p);
+// /*var app = */firebase.initializeApp(firebaseConfig);
 
-		if ( newJSON !== p1LastTimeJSON ) {
-			// console.log(newJSON + '!==' + p1LastTimeJSON);
-			console.log('sending '+ newJSON);
-			p1Ref.child('p').set(p1.p);
-			p1LastTimeJSON = newJSON;
-			p1LastTime = JSON.parse(p1LastTimeJSON);
-		}
-	}
 
-},1);
