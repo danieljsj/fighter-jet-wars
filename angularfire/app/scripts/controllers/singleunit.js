f31ddc3960282f0a6422cdbd15fe528e9f196e46
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-06-25 20:12:40 -0500

    bringing a stream of entities data into the singleUnitView

--- angularfire/app/scripts/controllers/singleunit.js
+++ angularfire/app/scripts/controllers/singleunit.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // PLAN: in player (or, temporarily, user), we save a "controlledEntityId --- I may have even written that somewhere; then entities check if their user is controlling them, and if not, they run their ais. you can have multiple windows open, multiple computers. also maybe a browser-instance can be kept; a 'console' object, and if a console is viewing a unit, then it skips over that unit when you toggle other consoles. actually that's goofy because sometimes you could infinite loop if you added a new console. better to let them select any."
+// UNLESS we want you to be able to have your user account controlling multiple... but I think that's perhaps cheating because then we're giving the user-controlled powerup to multiple planes on your team, allowing more fame points for your team. Which maybe is cool. An option for another day.
 
 /**
  * @ngdoc function
@@ -10,31 +11,38 @@
  * Controller of the angularfireApp
  */
 angular.module('angularfireApp')
-  .controller('SingleunitCtrl', function (Ref, user, $rootScope, LeafletMapService) {
+  .controller('SingleunitCtrl', function (Ref, user, $rootScope, $timeout, LeafletMapService) {
   	var vm = this;
 
-	var userRef = Ref.child('users/'+user.uid); // NOTE! At some point I may want to switch this to being playerRef since with multiple servers a user will associate to multiple "players", one per server.
 
 
+  	// SINGLE UNIT VIEW-MODEL:
 
+	var entitiesRef = Ref.child('entities');
 
-	// MAP:
+	entitiesRef.on('value', function(ss){
+		vm.entities = ss.val();
+  		$timeout(function(){$rootScope.$apply()});
+	});
+
 	
 	LeafletMapService.startMap('leafletMap')
 
 
-  	// SINGLE UNIT DISPLAY:
 
-  	userRef.on('value', function(snapshot){
-  		vm.p1 = snapshot.val();
-  		$rootScope.$apply();
-  	});
 
 
 
 
   	// SINGLE UNIT CONTROL:
 
+	var userRef = Ref.child('users/'+user.uid); // NOTE! At some point I may want to switch this to being playerRef since with multiple servers a user will associate to multiple "players", one per server.
+
+			// FOR DEBUGGING; won't need to see/subscribe to our own controls... unless we really want to...
+		  	userRef.on('value', function(ss){ // should eventually be Player, but haven't set it to be that yet
+		  		vm.player = ss.val();
+		  		$timeout(function(){$rootScope.$apply()});
+		  	});
 
 	//event listener
 	window.addEventListener('keydown', onKeyDown, false);
