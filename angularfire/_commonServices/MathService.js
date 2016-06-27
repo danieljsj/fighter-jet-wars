a26e8c82c8e94a7a3ef3b4479022de5a32ef33da
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-06-27 09:41:25 -0500

    theoretically finished TicksCalcService

---
+++ angularfire/_commonServices/MathService.js
@@ -0,0 +1,8 @@
+const standardGraduation = 0.1;
+
+class MathService {
+	roundTo(float,graduation){
+		graduation = graduation || standardGraduation;
+		return Math.round(rate/graduation)*graduation;
+	}
+}
--- angularfire/_commonServices/TicksCalcService.js
+++ angularfire/_commonServices/TicksCalcService.js
@@ -1,44 +1,45 @@
 
 const ticksPerSecond = 60; // this is the same as the maximum browser refresh rate, meaning that in smooth flow, 1 tick : 1 repaint . I was tempted to go with 100fps for rounding reasons, but I think we can declare all the actual game values in ticks per second either hardcoded-ly or at a one-time-init-func.
 
-
 let MathService = require('./MathService');
 
 
 
 
 
-
-
-
-class TickService {
-
-	constructor(){
-		
-	}
-
-	getFloat(){
-		return this.msToTicks(
-			(new Date()).getTime();
-		);		
-	}
+class TicksCalcService {
 
 	getLatest(){
 		return Math.floor(
 			this.getFloat()
 		);
 	}
+	getFloat(){
+		return this.msToTicks(
+			(new Date()).getTime();
+		);		
+	}
+
+
+	msToRoundedTicks(ms){
+		return Math.round(this.msToTicks(s));
+	}
+	sToRoundedTicks(s){
+		return Math.round(this.sToTicks(s));
+	}
+
 
 	msToTicks(ms){
 		return (ms/1000)*ticksPerSecond;
 	}
-
+	sToTicks(s){
+		return s*ticksPerSecond;
+	}
 
 
 	perSToPerTickRoundedTo(rate, graduation){
-		graduation = graduation || 1;
-		
+		return MathService.roundTo(rate,graduation);
 	}
 
 }
-module.exports = new TickService();
+module.exports = new TicksCalcService();
