51b4eb82829d58181ffcd3f5be73810bcca30144
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-06-27 10:26:02 -0500

    more work on ticks calc

--- angularfire/_commonServices/TicksCalcService.js
+++ angularfire/_commonServices/TicksCalcService.js
@@ -1,39 +1,50 @@
 
-const ticksPerSecond = 60; // this is the same as the maximum browser refresh rate, meaning that in smooth flow, 1 tick : 1 repaint . I was tempted to go with 100fps for rounding reasons, but I think we can declare all the actual game values in ticks per second either hardcoded-ly or at a one-time-init-func.
+///// SIDE-IDEA: stream of ticks is published like this: tickNum: ticksPerTick. as server load grows it will be like this 11111111020202020202003003003003003
+
+const ticksPerS = 60; // this is the same as the maximum browser refresh rate, meaning that in smooth flow, 1 tick : 1 repaint . I was tempted to go with 100fps for rounding reasons, but I think we can declare all the actual game values in ticks per second either hardcoded-ly or at a one-time-init-func.
+const ticksPerMs = ticksPerS / 1000;
+
+const sPerTick = 1/ticksPerS;
+const msPerTick = 1/ticksPerMs;
 
 let MathService = require('./MathService');
 
-
+function now(){	return (new Date()).getTime() }
 
 
 
 class TicksCalcService {
 
-	getLatest(){
+	latest(){
 		return Math.floor(
-			this.getFloat()
+			this.float()
 		);
 	}
-	getFloat(){
-		return this.msToTicks(
-			(new Date()).getTime();
-		);		
+	float(){
+		return this.msToTicks(now());		
+	}
+
+	msTillNext(){
+		return this.nextTime()-now();
+	}
+	nextTime(){
+		return Math.ceil(this.latest()*msPerTick); // ceil to aim high so that when we come back from our 'timeout', the last tick is barely in the past.
 	}
 
 
-	msToRoundedTicks(ms){
+	msToRoundTicks(ms){
 		return Math.round(this.msToTicks(s));
 	}
-	sToRoundedTicks(s){
+	sToRoundTicks(s){
 		return Math.round(this.sToTicks(s));
 	}
 
 
 	msToTicks(ms){
-		return (ms/1000)*ticksPerSecond;
+		return ms*ticksPerMs;
 	}
 	sToTicks(s){
-		return s*ticksPerSecond;
+		return s*ticksPerS;
 	}
 
 
