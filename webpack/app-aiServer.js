2ebd0c4fa8a00ce9c5b0e0a8cd1d23727d115885
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-07-25 17:58:10 -0500

    log2show in v3, node isn't even sending from root file (might have been true in v2 also)

--- webpack/app-aiServer.js
+++ webpack/app-aiServer.js
@@ -12,14 +12,14 @@ ControlsService.start();
 
 
 
-// const FirebaseRefService = require('./services/FirebaseRefService');
+const FirebaseRefService = require('./services/FirebaseRefService');
 
-// FirebaseRefService.initThen(function(){
-// 	const ref = FirebaseRefService.ref;
-// 	console.log('about to set foo to bar');
-// 	const testRef = ref.child('test/aiServerSetData');
-// 	testRef.push().set({foo:'bar'}, function(err){
-// 		if (err) throw err;
-// 		console.log('foo set to bar');
-// 	});
-// });
+FirebaseRefService.initThen(function(){
+	const ref = FirebaseRefService.ref;
+	console.log('about to set foo to bar (i.e. testing fb data send from root file of app...)');
+	const testRef = ref.child('test/aiServerSetData');
+	testRef.push().set({foo:'bar'}, function onComplete(err){
+		if (err) throw err;
+		console.log('foo set to bar (fb send from root file of app successful.)');
+	});
+});
