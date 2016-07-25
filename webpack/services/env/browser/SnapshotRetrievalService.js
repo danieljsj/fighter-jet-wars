f7a71d4244461636398d97cd72e7f6fe981953a0
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-07-25 16:11:32 -0500

    switched to node http module, and now I think we may be getting somewhere with errors? in any case it seems it has much better error logging

--- webpack/services/env/browser/SnapshotRetrievalService.js
+++ webpack/services/env/browser/SnapshotRetrievalService.js
@@ -1,20 +1,38 @@
 'use strict';
 
-var request = require('superagent');
+// var request = require('superagent');
+var http = require('http');
 
 function retrieveThen(cb){
 
-    // const url = 'http://localhost:4242/snapshot';
-    const url = 'http://127.0.0.1:4242/snapshot';
+    // request
+    //     .get(url)
+    //     .end(function(err, res){
+    //         if(err)throw(err);
+    //         var data = JSON.parse(res.text);
+    //         cb(data);
+    //     });
 
-    request
-        .get(url)
-        .end(function(err, res){
-            if(err)throw(err);
-            var data = JSON.parse(res.text);
-            cb(data);
+
+    const options = {
+        host: '127.0.0.1:4242',
+        path: '/snapshot '
+    }
+
+    http.get(options, function(res){
+        let str = '';
+        res.on('data', function (chunk) {
+            str += chunk;
         });
+        res.on('end', function () {
+            console.log('str',str);
+            cb(JSON.parse(str));
+        });
+    });
+
 }
+
+
 
 module.exports = {
     retrieveThen: retrieveThen,
