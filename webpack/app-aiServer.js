e5a4399a25fad6ea9a260862a52cb981e4fe45f2
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-07-25 13:58:55 -0500

    verified that coreServer can also set foo to bar in fb...

--- webpack/app-aiServer.js
+++ webpack/app-aiServer.js
@@ -8,4 +8,18 @@ const ControlsService = require('./services/env/aiServer/ControlsService');
 SimServ.start();
 initGDS();
 
-ControlsService.start();
+// ControlsService.start();
+
+
+
+const FirebaseRefService = require('./services/FirebaseRefService');
+
+FirebaseRefService.initThen(function(){
+	const ref = FirebaseRefService.ref;
+	console.log('about to set foo to bar');
+	const testRef = ref.child('test/aiServerSetData');
+	testRef.push().set({foo:'bar'}, function(err){
+		if (err) throw err;
+		console.log('foo set to bar');
+	});
+});
