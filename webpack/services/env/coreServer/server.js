89af6d35ae0c7a71b7014acdbf5d4bafe55f204c
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-07-25 16:22:45 -0500

    allowing credentials ... apparently ... in snapshot server

--- webpack/services/env/coreServer/server.js
+++ webpack/services/env/coreServer/server.js
@@ -9,7 +9,8 @@ function start(){
 	var app = express();
 
 	app.all('*', function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header('Access-Control-Allow-Credentials', true);
+	  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
 	  res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	  next();
 	 });
