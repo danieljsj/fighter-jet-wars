c81a0d0358ed8dc21db3acd6f7f62e3114e4b0f0
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-07-26 18:18:23 -0500

    deleted SnapshotStorageService

--- webpack/services/env/coreServer/SnapshotStorageService.js
+++
@@ -1,9 +0,0 @@
-
-const tickSnapshots = {};
-
-
-function saveSnapshot(snapshot){
-	
-	tickSnapshots[currTick] = this; // note: this will eventually clog the poo out of memory if we're not careful!!!!
-	
-}
