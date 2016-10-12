c4c2ae4a70c384ab629150d879ab0f124e6af7ec
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-10-12 18:09:25 -0500

    logging for jumpy planes

--- services/ToLog.js
+++ services/ToLog.js
@@ -4,48 +4,16 @@ class ToLog {
 
 	constructor() {
 
-
-		////// ALL, AS FALSES:
-
-		this.playerGen = false;
-		this.env = false;
-
-		this.snapshot = false;
-		this.snapshotFull = false;
-
-		this.p = false;
-		this.time = false;
-		this.ticks = false;
-		this.gD = false;
-
-
-		// io stuff:
-
-		this.command = false;
-		this.commandTimes = false;
-		this.commandSuccess = false;
-
-		this.serverSkippedTick = false;
-		this.serverSkippedTickTimes = false;
-		this.serverSkippedTickSuccess = false;
-
-		this.serverSnapshot = false;
-		this.serverSnapshotTimes = false;
-		this.serverSnapshotSuccess = false;	
-
-
-
-
 		/////// UNCOMMENT DESIRED TRUES:
 
 		this.playerGen = true;
-		this.env = true;
+		// this.env = true;
 
 
 		this.snapshot = true;
-		this.snapshotFull = true;
+		// this.snapshotFull = true;
 
-		this.p = true;
+		// this.p = true;
 		// this.time = true;
 		// this.ticks = true;
 		// this.gD = true;
@@ -54,8 +22,8 @@ class ToLog {
 		// // // io stuff:
 
 		this.command = true;
-		this.commandTimes = true;
-		this.commandSuccess = true;
+		// this.commandTimes = true;
+		// this.commandSuccess = true;
 
 		// this.serverSkippedTick = true;
 		// this.serverSkippedTickTimes = true;
