ecfbcc0bc8f0b08d86fc54c62552b1b9776d322e
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2017-07-11 14:46:55 -0500

    deepDiff only stores diffy branches

--- notes/tests/deepDiff.js
+++ notes/tests/deepDiff.js
@@ -1,30 +1,32 @@
+'use strict';
 console.log("____________________");
 
-function is_scalar(obj){return (/string|number|boolean/).test(typeof obj);}
 
-ob1 = {
+const ob1 = {
 	"num":1,
 	"ob":{
 		"deepnumA":1,
-		"deepnumB":3,
+		"deepnumB":1,
 	}
 }
 
-ob2 = {
-	"num":2,
+const ob2 = {
+	"num":1,
 	"ob":{
-		"deepnumA":2,
-		"deepnumB":3,
+		"deepnumA":1,
+		"deepnumB":2,
 	}
 }
 
+function is_scalar(obj){return (/string|number|boolean/).test(typeof obj);}
 
 function iterateAndReportDeepDifferences(ob1,ob2){
 	console.log("iterateAndReportDeepDifferences on")
 	console.log(ob1);
 	console.log(ob2);
 	const diffMap = {};
-	for (key in ob1) {
+	let hasDiffs = false;
+	for (const key in ob1) {
 		console.log("comparing",key,ob1[key],ob2[key]);
 		if (ob1[key]===ob2[key]){
 			console.log("items are identical; ignoring them");
@@ -32,16 +34,19 @@ function iterateAndReportDeepDifferences(ob1,ob2){
 			if (is_scalar(ob1[key])&&is_scalar(ob2[key])){
 				console.log("diffed are scalar; saving ob2[key] into diffMap[key]: ",ob2[key]); // whoah, this keep
 				diffMap[key] = ob2[key];
+				hasDiffs = true;
 			} else {
 				const subDiffMap = iterateAndReportDeepDifferences(ob1[key],ob2[key]); 
-				console.log("saving diffMap into",key); // whoah, this keep
-				diffMap[key] = subDiffMap;
+				if (subDiffMap){
+					hasDiffs = true;
+					console.log("saving diffMap into",key); // whoah, this keep
+					diffMap[key] = subDiffMap;
+				}
 			}
 		}
 	}
-	return diffMap;
+	return hasDiffs && diffMap;
 }
-
 
 const diffMap = iterateAndReportDeepDifferences(ob1,ob2);
 
